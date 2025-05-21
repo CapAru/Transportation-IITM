45dbbb8fb02eed4be04e372f292c3cf0cf8db138
Arunava Chakrabarty <137680973+CapAru@users.noreply.github.com>
2025-05-21 20:51:39 +0530

    Created Login and Sign up pages

---
+++ src/app/login/page.js
@@ -0,0 +1,50 @@
+import Link from "next/link";
+
+export default function Login(){
+    return (
+        <div className="flex items-center min-h-screen bg-gray-100 w-full">
+            <img src="/Background.png" className="h-screen" />
+            <div className="flex flex-col items-center justify-center bg-gradient-to-br from-purple-900 via-blue-950 to-gray-900 px-8 py-8 w-full min-h-screen">
+                <div className="w-full p-10 text-white">
+                    <h1 className="text-5xl my-4">Login</h1>
+                    <form className="flex flex-col items-center w-full">
+                        <div className="flex flex-col items-start w-full my-3">
+                            <label htmlFor="email" className="mb-1">
+                                Email
+                            </label>
+                            <input
+                                type="email"
+                                id="email"
+                                placeholder="example@mail.com"
+                                className="border-blue-600 border-2 rounded-lg py-2 px-4 text-md w-full bg-gray-100 placeholder-gray-500"
+                            />
+                        </div>
+                        <div className="flex flex-col items-start w-full my-3">
+                            <label htmlFor="password" className="mb-1">
+                                Password
+                            </label>
+                            <input
+                                type="password"
+                                id="password"
+                                placeholder="password"
+                                className="border-blue-600 border-2 rounded-lg py-2 px-4 text-md w-full bg-gray-100 placeholder-gray-500"
+                            />
+                        </div>
+                        <button
+                            type="submit"
+                            className="bg-blue-400 text-white py-2 px-4 rounded-lg mt-6 w-full"
+                        >
+                            Login
+                        </button>
+                        <p className="mt-4">
+                            Don't have an account?{" "}
+                            <Link href="/signup" className="text-blue-400">
+                                Sign Up
+                            </Link>
+                        </p>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+}
